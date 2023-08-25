e99e16c4f8dbcfe1ac26b3e256d95d4e213beb89
LightFloatCloud <chenyexin123@foxmail.com>
2023-08-25 16:04:27 +0800

    2023.8.25 多线程学习第一课

---
+++ src/issue1114.cpp
@@ -0,0 +1,44 @@
+/*
+多线程学习第一课
+*/
+
+
+#include <functional>
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+using namespace std;
+
+class Foo {
+protected:
+    std::mutex _mutex1;
+    std::mutex _mutex2;
+public:
+    Foo() {
+        _mutex1.lock();
+        _mutex2.lock();
+    }
+
+    void first(function<void()> printFirst) {
+        
+        // printFirst() outputs "first". Do not change or remove this line.
+        printFirst();
+        _mutex1.unlock();
+    }
+
+    void second(function<void()> printSecond) {
+        std::unique_lock<std::mutex> lock1(_mutex1);
+        // printSecond() outputs "second". Do not change or remove this line.
+        printSecond();
+        _mutex2.unlock();
+    }
+
+    void third(function<void()> printThird) {
+        std::unique_lock<std::mutex> lock2(_mutex2);
+        // printThird() outputs "third". Do not change or remove this line.
+        printThird();
+    }
+};
+
+
